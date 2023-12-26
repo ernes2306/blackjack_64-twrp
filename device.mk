1688a01383d1bb026431d0019978c801ce24c7f9
ernes2306 <114512513+ernes2306@users.noreply.github.com>
2023-12-26 19:44:47 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,14 +16,16 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.mt6765 \
-    bootctrl.mt6765.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl
+    bootctrl.mt6765
 
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    #bootctrl.mt6765 \
+    #libgptutils \
+    #libz \
+    #libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
