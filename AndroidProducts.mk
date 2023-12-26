f9f85358a5106d273b23bf4a4cadc9465658fce5
ernes2306 <sejas.ernesto.et21.21@gmail.com>
2023-12-26 16:54:34 -0300

    update

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 SebaUbuntu's omni device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_blackjack_64.mk
+    $(LOCAL_DIR)/omni_blackjack_64.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_blackjack_64-user \
-    twrp_blackjack_64-userdebug \
-    twrp_blackjack_64-eng
+    omni_blackjack_64-user \
+    omni_blackjack_64-userdebug \
+    omni_blackjack_64-eng
